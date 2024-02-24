c89709ef93e3fd12274ed80d8ab58d9db0799057
Ibazone <159165981+Ibazone@users.noreply.github.com>
2024-02-24 15:29:41 -0500

    Update BookingController.cs
    Added FlightBooking to database;
    will add CarRental and Hotel Booking Once error are fixed

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using WebApplication4.Data;
 using WebApplication4.Models;
 
 namespace WebApplication4.Controllers
 {
-	public class BookingController : Controller
-	{
-		private readonly AppDbContext _db;
+    public class BookingController : Controller
+    {
+        private readonly AppDbContext _db;
 
-		public BookingController(AppDbContext db)
-		{
+        public BookingController(AppDbContext db)
+        {
 
-			_db = db;
+            _db = db;
 
-		}
+        }
         public ActionResult Flight()
         {
             ViewBag.Provinces = new SelectList(new[]
@@ -50,97 +50,144 @@ namespace WebApplication4.Controllers
 
 
         public ActionResult CarRental()
-		{
-			var carRentals = _db.Listings.Where(l => l.Type == "Car").ToList();
+        {
+            var carRentals = _db.Listings.Where(l => l.Type == "Car").ToList();
 
-			ViewBag.CarModels = new SelectList(carRentals.Select(l => l.Name).Distinct());
+            ViewBag.CarModels = new SelectList(carRentals.Select(l => l.Name).Distinct());
 
-			return View();
-		}
+            return View();
+        }
 
-		[HttpPost]
-		public ActionResult RentCar(CarRentalModel model)
-		{
-			if (ModelState.IsValid)
-			{
+        [HttpPost]
+        public ActionResult RentCar(CarRentalModel model)
+        {
+            if (ModelState.IsValid)
+            {
 
 
-				return RedirectToAction("CarRentalConfirmation", "Booking", model);
-			}
+                return RedirectToAction("CarRentalConfirmation", "Booking", model);
+            }
 
             var carRentals = _db.Listings.Where(l => l.Type == "Car").ToList();
             ViewBag.CarModels = new SelectList(carRentals.Select(l => l.Name).Distinct());
 
-         
+
 
             return View("CarRental", model);
-		}
+        }
 
-		public ActionResult CarRentalConfirmation(CarRentalModel model)
-		{
-			return View(model);
-		}
+        public ActionResult CarRentalConfirmation(CarRentalModel model)
+        {
+            return View(model);
+        }
 
-		public ActionResult Hotel()
-		{
-			var hotels = _db.Listings.Where(l => l.Type == "Hotel").ToList();
+        public ActionResult Hotel()
+        {
+            var hotels = _db.Listings.Where(l => l.Type == "Hotel").ToList();
 
-			ViewBag.Provinces = new SelectList(new[]
-			{
-				"Alberta", "British Columbia", "Manitoba", "New Brunswick",
-				"Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island",
-				"Quebec", "Saskatchewan"
-			});
+            ViewBag.Provinces = new SelectList(new[]
+            {
+                "Alberta", "British Columbia", "Manitoba", "New Brunswick",
+                "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island",
+                "Quebec", "Saskatchewan"
+            });
 
-			ViewBag.Hotels = new SelectList(hotels.Select(l => l.Name).Distinct());
+            ViewBag.Hotels = new SelectList(hotels.Select(l => l.Name).Distinct());
 
 
-			ViewBag.Guests = new SelectList(new[]
-			{
-				"1", "2", "3", "4"
-			});
+            ViewBag.Guests = new SelectList(new[]
+            {
+                "1", "2", "3", "4"
+            });
 
-			return View("Hotel");
-		}
+            return View("Hotel");
+        }
 
-		[HttpPost]
-		public ActionResult BookHotel(HotelBookingModel model)
-		{
-			if (ModelState.IsValid)
-			{
+        [HttpPost]
+        public ActionResult BookHotel(HotelBookingModel model)
+        {
+            if (ModelState.IsValid)
+            {
 
-				return RedirectToAction("HotelConfirmation", "Booking", model);
-			}
+                return RedirectToAction("HotelConfirmation", "Booking", model);
+            }
 
-			ViewBag.Provinces = new SelectList(new[]
-			{
-				"Alberta", "British Columbia", "Manitoba", "New Brunswick",
-				"Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island",
-				"Quebec", "Saskatchewan"
-			});
+            ViewBag.Provinces = new SelectList(new[]
+            {
+                "Alberta", "British Columbia", "Manitoba", "New Brunswick",
+                "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island",
+                "Quebec", "Saskatchewan"
+            });
 
-			ViewBag.Hotels = new SelectList(new[]
-			{
-				"Confort Inn", "Best Western", "Marriott", "Hilton"
-			});
+            ViewBag.Hotels = new SelectList(new[]
+            {
+                "Confort Inn", "Best Western", "Marriott", "Hilton"
+            });
 
-			ViewBag.Guests = new SelectList(new[]
-			{
-				"1", "2", "3", "4"
-			});
+            ViewBag.Guests = new SelectList(new[]
+            {
+                "1", "2", "3", "4"
+            });
 
-			return View("Hotel", model);
-		}
+            return View("Hotel", model);
+        }
 
-		public ActionResult HotelConfirmation(HotelBookingModel model)
-		{
-			return View(model);
-		}
+        public ActionResult HotelConfirmation(HotelBookingModel model)
+        {
+            return View(model);
+        }
 
-		public IActionResult Listings()
-		{
-			return View();
-		}
+        public IActionResult Listings()
+        {
+            return View();
+        }
 
-	}
-}
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(FlightBookingModel Flight)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.FlightBookings.Add(Flight);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Flight);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var project = _db.FlightBookings.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var listing = _db.FlightBookings.FirstOrDefault(p => p.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+            return View(Flight);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int FlightID)
+        {
+            var flight = _db.FlightBookings.Find(FlightID);
+            if (flight != null)
+            {
+                _db.FlightBookings.Remove(flight);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
